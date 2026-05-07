99debbf6502ca5a272ab5992b49a899cd18776ad
Sankarraja <sankarrajanagarajan@gmail.com>
2026-05-07 17:31:56 +0530

    changes on the query

--- DPP.InternalWebhookHost.Infrastructure/DbQueries.cs
+++ DPP.InternalWebhookHost.Infrastructure/DbQueries.cs
@@ -1,8 +1,8 @@
 ﻿namespace DPP.PartnerPaymentIntegration.Infrastructure
 {
-	public static class DbQueries
-	{
-		public const string WebhookLogSave = @"
+    public static class DbQueries
+    {
+        public const string WebhookLogSave = @"
             INSERT INTO [CoreTransaction].[dbo].[WebHookPayloads]
             (
                 [DateReceived],
@@ -16,8 +16,21 @@
                 @Payload
             );";
 
-		public const string GetWebhoolLogs = @" SELECT COUNT(*) FROM WebhookPayloads  WHERE (@StartDateTime IS NULL OR DateTimeReceived >= @StartDateTime) AND (@EndDateTime IS NULL OR DateTimeReceived <= @EndDateTime);
-              SELECT Id, DateTimeReceived as ReceivedAt, Payload as Data FROM WebhookPayloads WHERE (@StartDateTime IS NULL OR DateTimeReceived >= @StartDateTime)
-          AND (@EndDateTime IS NULL OR DateTimeReceived <= @EndDateTime) ORDER BY DateTimeReceived DESC OFFSET @Offset ROWS FETCH NEXT @PageSize ROWS ONLY;";
-	}
+
+
+        public const string GetWebhoolLogs = @"
+                SELECT COUNT(*) FROM WebhookPayloads WITH (NOLOCK)
+                WHERE (@StartDateTime IS NULL OR DateTimeReceived >= @StartDateTime)
+                AND (@EndDateTime IS NULL OR DateTimeReceived <= @EndDateTime);
+
+                SELECT 
+                        Id, 
+                        DateTimeReceived AS ReceivedAt, 
+                        Payload AS Data 
+                FROM WebhookPayloads WITH (NOLOCK)
+                WHERE (@StartDateTime IS NULL OR DateTimeReceived >= @StartDateTime)
+                AND (@EndDateTime IS NULL OR DateTimeReceived <= @EndDateTime)
+                ORDER BY DateTimeReceived DESC
+                OFFSET @Offset ROWS FETCH NEXT @PageSize ROWS ONLY;";
+    }
 }
--- DPP.InternalWebhookHost.Infrastructure/Repositories/WebhookRepository.cs
+++ DPP.InternalWebhookHost.Infrastructure/Repositories/WebhookRepository.cs
@@ -26,25 +26,6 @@ public class WebhookRepository : IWebhookRepository
 
     public async Task<(int TotalCount, IEnumerable<dynamic> Items)> GetWebhookReportAsync( DateTime? start,DateTime? end,int pageNumber , int pageSize , CancellationToken cancellationToken)
     {
-        //const string sql = @"
-        //    SELECT COUNT(*) FROM WebhookPayloads 
-        //    WHERE (@StartDateTime IS NULL OR DateTimeReceived >= @StartDateTime) 
-        //      AND (@EndDateTime IS NULL OR DateTimeReceived <= @EndDateTime);
-
-        //    SELECT Id, DateTimeReceived as ReceivedAt, Payload as Data 
-        //    FROM WebhookPayloads 
-        //    WHERE (@StartDateTime IS NULL OR DateTimeReceived >= @StartDateTime)
-        //      AND (@EndDateTime IS NULL OR DateTimeReceived <= @EndDateTime) 
-        //    ORDER BY DateTimeReceived DESC 
-        //    OFFSET @Offset ROWS FETCH NEXT @PageSize ROWS ONLY;";
-
-        //using var multi = await _dbConnection.QueryMultipleAsync(sql, new
-        //{
-        //    StartDateTime = start,
-        //    EndDateTime = end,
-        //    Offset = (pageNumber - 1) * pageSize,
-        //    PageSize = pageSize
-        //});
         using var conn = await dbConnection.GetCoreTransactionConnection(cancellationToken);
         int validPageSize = pageSize > 0 ? pageSize : 10;
         int validOffset = (pageNumber > 0 ? pageNumber - 1 : 0) * validPageSize;
