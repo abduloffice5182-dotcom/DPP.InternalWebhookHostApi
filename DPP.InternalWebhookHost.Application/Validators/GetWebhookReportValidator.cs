c9ca3d3574958b404055f6b0a630d4bcc9c54f8b
Sankarraja <sankarrajanagarajan@gmail.com>
2026-05-11 19:14:25 +0530

    chnages on the structure based on CQRSpattern

---
+++ DPP.InternalWebhookHost.Application/Validators/GetWebhookReportValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using DPP.InternalWebhookHost.Application.Operations.Queries.Requests;
+
+namespace DPP.InternalWebhookHost.Application.Validators;
+
+public class GetWebhookReportValidator : AbstractValidator<GetWebhookReportQuery>
+{
+    public GetWebhookReportValidator()
+    {
+        this.ApplyDateRules();
+        this.ApplyPagingRules();
+    }
+}
