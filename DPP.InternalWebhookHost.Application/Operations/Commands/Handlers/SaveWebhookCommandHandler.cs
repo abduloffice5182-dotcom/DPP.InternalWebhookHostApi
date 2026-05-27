8a83ac52419d1f5379052ed82b2ddffe4772963c
Rameshkumar.Krishnas <t476797@deluxe.com>
2026-05-27 09:46:27 +0530

    rabbimq flag handled

--- DPP.InternalWebhookHost.Application/Operations/Commands/Handlers/SaveWebhookCommandHandler.cs
+++ DPP.InternalWebhookHost.Application/Operations/Commands/Handlers/SaveWebhookCommandHandler.cs
@@ -6,16 +6,30 @@ public class SaveWebhookCommandHandler : IRequestHandler<SaveWebhookCommand>
 {
 	readonly IRabbitMqProducerService rabbitMqProducer;
 	readonly IConfiguration configuration;
+	readonly IWebhookRepository webhookRepository;
 
-	public SaveWebhookCommandHandler(IConfiguration configuration, IRabbitMqProducerService rabbitMqProducer)
+	public SaveWebhookCommandHandler(IConfiguration configuration, IRabbitMqProducerService rabbitMqProducer, IWebhookRepository webhookRepository)
 	{
 		this.rabbitMqProducer = rabbitMqProducer;
 		this.configuration = configuration;
+		this.webhookRepository = webhookRepository;
 	}
 
 	public async Task Handle(SaveWebhookCommand request, CancellationToken cancellationToken)
-	{ 
+	{
+		if (configuration.GetValue<bool?>("RabbitMQ:EnableRabbitMQ") ?? false)
+		{
+			await rabbitMqProducer.PublishAsync(configuration["RabbitMQ:Exchange"]!, configuration["WebhookCreatedQueue"]!, configuration.GetValue<string>("RabbitMQ:WebhookCreatedQueueRoutingKey")!, request);
 
-		await rabbitMqProducer.PublishAsync(configuration["RabbitMQ:Exchange"]!, configuration["WebhookCreatedQueue"]!, configuration.GetValue<string>("RabbitMQ:WebhookCreatedQueueRoutingKey")!, request); 
+		}
+		else
+		{
+			await webhookRepository.WebhooklLogSave(new SaveWebhookPayloadsRequest(
+				 Payload: request.Payload,
+				 EndpointId: request.EndpointId
+				)
+			 , cancellationToken);
+		}
+
 	}
 }
